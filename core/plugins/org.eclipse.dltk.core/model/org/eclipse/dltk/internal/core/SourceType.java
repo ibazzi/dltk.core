6c9e659fabc47091375b13b9d237ef35ca7627f2
ibazzi <ibazzi@qq.com>
2017-01-19 15:10:25 +0800

    FIX: failed to display type hierarchy for class

--- core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/SourceType.java
+++ core/plugins/org.eclipse.dltk.core/model/org/eclipse/dltk/internal/core/SourceType.java
@@ -19,6 +19,7 @@ import org.eclipse.dltk.compiler.CharOperation;
 import org.eclipse.dltk.core.CompletionRequestor;
 import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.core.DLTKLanguageManager;
+import org.eclipse.dltk.core.IBuildpathEntry;
 import org.eclipse.dltk.core.IField;
 import org.eclipse.dltk.core.IMethod;
 import org.eclipse.dltk.core.IModelElement;
@@ -477,15 +478,37 @@ public class SourceType extends NamedMember implements IType {
 	private IDLTKSearchScope createReferencingProjectsScope() {
 
 		IScriptProject scriptProject = getScriptProject();
-		IProject project = scriptProject.getProject();
-		IProject[] referencingProjects = project.getReferencingProjects();
+		IProject[] projects = scriptProject.getProject().getWorkspace()
+				.getRoot().getProjects();
+		List<IProject> result = new ArrayList<>(projects.length);
+		for (int i = 0; i < projects.length; i++) {
+			IProject project = projects[i];
+			if (!project.isAccessible())
+				continue;
+			IScriptProject p = DLTKCore.create(project);
+			if (p.equals(scriptProject)) {
+				result.add(project);
+				continue;
+			}
+			IBuildpathEntry[] references;
+			try {
+				references = p.getRawBuildpath();
+				for (int j = 0; j < references.length; j++) {
+					if (references[j].getPath()
+							.equals(scriptProject.getPath())) {
+						result.add(projects[i]);
+						break;
+					}
+				}
+			} catch (ModelException e) {
+				DLTKCore.error(e);
+			}
+		}
 
-		List<IScriptProject> scriptProjects = new ArrayList<>(
-				referencingProjects.length + 1);
-		scriptProjects.add(scriptProject);
+		List<IScriptProject> scriptProjects = new ArrayList<>(result.size());
+		for (int i = 0; i < result.size(); ++i) {
+			IProject p = result.get(i);
 
-		for (int i = 0; i < referencingProjects.length; ++i) {
-			IProject p = referencingProjects[i];
 			if (p.isAccessible()) {
 				scriptProjects.add(DLTKCore.create(p));
 			}
@@ -493,7 +516,7 @@ public class SourceType extends NamedMember implements IType {
 		return SearchEngine.createSearchScope(
 				scriptProjects
 						.toArray(new IModelElement[scriptProjects.size()]),
-				false, DLTKLanguageManager.getLanguageToolkit(this));
+				DLTKLanguageManager.getLanguageToolkit(this));
 	}
 
 	/**
